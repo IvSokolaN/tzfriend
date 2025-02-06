b3b6a90eb486a9c5ec0420d3dcc20997258ac452
IvSokolaN <sokolov.wd@bk.ru>
2025-02-06 13:09:44 +0800

    add models Article, Tag and migrations

---
+++ app/Models/Article.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'preview_image',
+        'published_at',
+        'user_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
