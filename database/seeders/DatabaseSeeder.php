f2b899653615d8225cebaf5ec79ac1e596e39b8d
IvSokolaN <sokolov.wd@bk.ru>
2025-02-07 19:05:46 +0800

    add seeder and factory for roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\ArticleTag;
-use App\Models\Tag;
+use App\Models\RoleUser;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -23,8 +23,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             TagSeeder::class,
+            RoleSeeder::class,
         ]);
 
         ArticleTag::factory(50)->create();
+        RoleUser::factory(20)->create();
     }
 }
